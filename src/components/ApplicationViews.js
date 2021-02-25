cefe18e826246e5056e1a442646187fe098d06e5
MonroeMegalonyx <7621080+MonroeMegalonyx@users.noreply.github.com>
2021-02-25 15:08:41 -0500

    Implemented navbar

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -1,7 +1,10 @@
 import React from "react"
 import { Route } from "react-router-dom"
 import { Home } from "./Home"
+import { LocationCard } from "./location/LocationCard"
 import { AnimalCard } from "./animal/AnimalCard"
+import { CustomerCard } from "./customer/CustomerCard"
+import { EmployeeCard } from "./employee/EmployeeCard"
 
 export const ApplicationViews = () => {
     return (
@@ -11,9 +14,24 @@ export const ApplicationViews = () => {
                 <Home />
             </Route>
 
+            {/* Render the animal list when http://localhost:3000/locations */}
+            <Route path="/Locations">
+                <LocationCard />
+            </Route>
+
             {/* Render the animal list when http://localhost:3000/animals */}
             <Route path="/animals">
                 <AnimalCard />
+            </Route>
+
+            {/* Render the animal list when http://localhost:3000/customers */}
+            <Route path="/customers">
+                <CustomerCard />
+            </Route>
+
+            {/* Render the animal list when http://localhost:3000/employees */}
+            <Route path="/employees">
+                <EmployeeCard />
             </Route>
         </>
     )
