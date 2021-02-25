23f387b36d0a18e3e991f1fe7b1cf0df9ec600a4
MonroeMegalonyx <7621080+MonroeMegalonyx@users.noreply.github.com>
2021-02-25 15:03:51 -0500

    Added example navbar

--- src/index.js
+++ src/index.js
@@ -12,11 +12,14 @@
 // );
 import React from "react"
 import ReactDOM from "react-dom"
+import { BrowserRouter as Router } from "react-router-dom"
 import { Kennel } from "./components/Kennel"
 
 ReactDOM.render(
   <React.StrictMode>
-    <Kennel />
+    <Router>
+        <Kennel />
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
